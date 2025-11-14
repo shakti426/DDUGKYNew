f29dafd7c10392d0d4240341d081f8ece9fb3eca
Tabish <tjamal333@gmail.com>
2025-11-14 11:40:29 +0530

    all data set and submit

--- app/src/main/java/com/deendayalproject/network/ApiService.kt
+++ app/src/main/java/com/deendayalproject/network/ApiService.kt
@@ -237,7 +237,7 @@ interface ApiService {
     suspend fun insertQTeamVerification(@Body request: TcQTeamInsertReq) : Response<InsertTcGeneralDetailsResponse>
 
 
-    @POST(value ="insertQTeamVerification")
+    @POST(value ="insertSrlmVerification")
     suspend fun insertSrlmVerification(@Body request: TcQTeamInsertReq) : Response<InsertTcGeneralDetailsResponse>
 
     @POST(value ="trainingCenterFinalInsert")
